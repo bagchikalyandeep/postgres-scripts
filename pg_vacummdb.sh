d47ef5c7daa608da852eb06807624a6b73007249
Kalyandeep Bagchi <bagchikalyandeep@gmail.com>
2022-07-22 16:13:29 +0530

    Create pg_vacummdb.sh

---
+++ pg_vacummdb.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# PostgreSQL vacuum script for E2C DB Team
+# Prepared by Kalyandeep Bagchi
+# version 1.0
+# usage <path_to_vacuumdb.sh>
+set -x
+
+# rotating vacuumdb logs
+vacuumRotate(){
+        cd /u05/pgcluster/$pgVer/exp/e2c_vacuumdb_logs/`hostname -f`/
+        find . -type f -name "*.log" -mtime +30 -delete
+        exit 0;
+}
+
+# check if PostgreSQL process is running
+pgProc=`pgrep -u e2cpostgre -fa -- -D`
+[[ -z $pgProc ]] && pgState=0 || pgState=1
+
+case $pgState in
+  0)
+    exit 1;;
+  1)
+    # Environment details
+        host=`hostname -f`
+        vacuumdbPath=`find /local -type f -name "vacuumdb"`
+        pgVer=`$vacuumdbPath --version | awk '{print $(NF)}' | cut -d '.' -f 1`
+        pgPort=`netstat -plunt | grep postgres | egrep -v tcp[6] | awk '{print $4}' | rev  | cut -d ':' -f 1 | rev`
+        vacummLog=/u05/pgcluster/$pgVer/exp/e2c_vacuumdb_logs/$host/vacuumLogs-"`date +"%d-%m-%Y"-%H-%M-%S`.log"
+        touch $vacummLog
+
+        # check if Postgres instance is primary or not
+        pgState=`psql postgresql://localhost:5454/postgres -tc "select pg_is_in_recovery();" |head -1`
+
+        if [ $pgState == t ]; then # when PostgreSQL node is Secondary / Read-only
+                echo "PG Node not primary, aborting!"
+                exit 0;
+
+        elif [ $pgState == f ];then # when PostgreSQL node is Primary / non-read-only
+                $vacuumdbPath -azv --host=`hostname -f` --port=$pgPort 2>&1 | tee $logFile
+                # calling vacuumdb logs
+                vacuumRotate
+        else
+                echo "Unable to determine state of the PostgreSQL node, aborting!"
+                exit 1;
+        fi;;
+esac
